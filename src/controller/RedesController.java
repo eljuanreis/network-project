549478bb3f462edeeaf2429813f66d99816c8295
Juan Reis <contatojuanzito@gmail.com>
2023-02-27 17:11:50 -0300

    update project

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.swing.JOptionPane;
+
 public class RedesController {
 
 	static String soWindowLabel = "windows";
@@ -15,7 +17,7 @@ public class RedesController {
 	
 	public void ipConfig() {
 		String so = this.getOS().toLowerCase();
-
+		
 		try {
 			if (so.contains(soWindowLabel)) {
 				Process p = Runtime.getRuntime().exec("ipconfig");
@@ -42,6 +44,9 @@ public class RedesController {
 				String[] linhas = todasLinhas.toString().split("\n");
 				int tamanhoLinhas = linhas.length;
 				
+				// Guardar as linhas para exibir no JOptionPane
+				StringBuffer linhaToPane = new StringBuffer();
+				
 				for (int i = 0; i < tamanhoLinhas; i++) {
 					
 					if (i == tamanhoLinhas - 1) {
@@ -49,10 +54,12 @@ public class RedesController {
 					}
 					
 					if (linhas[i+1].contains("IPv4")) {
-						System.out.println(linhas[i]);
-						System.out.println(linhas[i+1]);
+						linhaToPane.append(linhas[i] + "\n");
+						linhaToPane.append(linhas[i+1] + "\n");
 					}
 				}
+				
+				JOptionPane.showMessageDialog(null, linhaToPane.toString());
 			}
 			
 			if (so.contains(soLinuxLabel)) {
@@ -80,6 +87,9 @@ public class RedesController {
 				String[] linhas = todasLinhas.toString().split("\n");
 				int tamanhoLinhas = linhas.length;
 				
+				// Guardar as linhas para exibir no JOptionPane
+				StringBuffer linhaToPane = new StringBuffer();
+				
 				for (int i = 0; i < tamanhoLinhas; i++) {
 					
 					if (i == tamanhoLinhas - 1) {
@@ -88,10 +98,16 @@ public class RedesController {
 					
 					if (linhas[i+1].contains("inet")) {
 						System.out.println(linhas[i]);
-						System.out.println(linhas[i+1]);
+						
+						if (linhas[i+1].contains("inet6") == false) {
+							String[] removeStrings = linhas[i+1].split("netmask");
+
+							linhaToPane.append(removeStrings[0]);
+						}
 					}
 				}
 				
+				JOptionPane.showMessageDialog(null, linhaToPane.toString());
 			}			
 		} catch (Exception e) {
 			System.err.println(e.getLocalizedMessage());
@@ -103,7 +119,8 @@ public class RedesController {
 		
 		try {
 			if (so.contains(soWindowLabel)) {
-				
+				JOptionPane.showMessageDialog(null, "Aguarde... Estamos realizando a verificação.");
+	
 				Process p = Runtime.getRuntime().exec("PING -4 -n 10 www.google.com.br");
 				
 				InputStream fluxo = p.getInputStream();
@@ -128,17 +145,24 @@ public class RedesController {
 				String[] linhas = todasLinhas.toString().split("tempo=");
 				int tamanhoLinhas = linhas.length;
 				
+				// Guardar as linhas para exibir no JOptionPane
+				StringBuffer linhaToPane = new StringBuffer();
+				
 				for (int i = 0; i < tamanhoLinhas; i++) {
 					if (linhas[i].contains("ms")) {
 						String[] line = linhas[i].split("TTL");
 						
-						System.out.println(line[0]);
+						linhaToPane.append(line[0] + "\n");
 					}
 				}
+				
+				JOptionPane.showMessageDialog(null, linhaToPane.toString());
 			}
 			
 			if (so.contains(soLinuxLabel)) {
-				Process p = Runtime.getRuntime().exec("PING -4 -n 10 www.google.com.br");
+				JOptionPane.showMessageDialog(null, "Aguarde... Estamos realizando a verificação.");
+				
+				Process p = Runtime.getRuntime().exec("ping -4 -c 10 www.google.com.br");
 				
 				InputStream fluxo = p.getInputStream();
 				InputStreamReader leitor = new InputStreamReader(fluxo);
@@ -161,14 +185,18 @@ public class RedesController {
 				
 				String[] linhas = todasLinhas.toString().split("time=");
 				int tamanhoLinhas = linhas.length;
+
+				// Guardar as linhas para exibir no JOptionPane
+				StringBuffer linhaToPane = new StringBuffer();
 				
 				for (int i = 0; i < tamanhoLinhas; i++) {
 					if (linhas[i].contains("ms")) {
-						String[] line = linhas[i].split("TTL");
-						
-						System.out.println(line[0]);
+						String[] line = linhas[i].split("\n");
+						linhaToPane.append(line[0] + "\n");
 					}
 				}
+				
+				JOptionPane.showMessageDialog(null, linhaToPane.toString());
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,14 +1,88 @@
 package view;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Cursor;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
 import controller.RedesController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Main {
+import net.miginfocom.swing.MigLayout;
 
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		RedesController redesController = new RedesController();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setTitle("Network Manager");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 270, 181);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[100px][204px]", "[23px][23px][23px][]"));
 		
-		//redesController.ipConfig();
-		redesController.ping();
+		JLabel lblNewLabel = new JLabel("System: \n" + System.getProperty("os.name"));
+		contentPane.add(lblNewLabel, "cell 0 0,alignx left,aligny center");
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 1 0,alignx left,aligny top");
+		panel.setLayout(new GridLayout(0, 4, 0, 0));
+		
+		JButton btnNewButton = new JButton("Config IP");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RedesController controller = new RedesController();
+				controller.ipConfig();
+			}
+		});
+		contentPane.add(btnNewButton, "cell 0 1 2 1,growx,aligny top");
+		
+		JButton btnNewButton_1 = new JButton("Ping Google");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RedesController controller = new RedesController();
+				contentPane.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+				controller.ping();
+				contentPane.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+
+			}
+		});
+		contentPane.add(btnNewButton_1, "cell 0 2 2 1,growx,aligny top");
+		
+		JButton btnNewButton_2 = new JButton("Exit");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(EXIT_ON_CLOSE);
+			}
+		});
+		contentPane.add(btnNewButton_2, "cell 0 3 2 1,growx");
 	}
 
 }
